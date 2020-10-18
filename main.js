fc1913e4cdbbb05b36442a53db1596cf435c8b55
eliot_cheng <eliot_cheng@gss.com.tw>
2020-10-18 22:41:41 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const selector = "div.tw-full-height.tw-relative.tw-z-above > div > div > div > button";
+const handler = function () {
+	const el = document.querySelector(selector);
+	if (el) {
+		el.click();
+	}
+}
+
+setInterval(handler, 5000);
